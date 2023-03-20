ad8c02d96b380769355d2ffe931af8a570019825
keplerfrequency <keplerfrequency@gmail.com>
2023-03-20 21:38:10 +0100

    Changed folder structure

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 // Function to generate the HTML for the photo carousel
 function generateCarouselHTML(photos) {
-    let html = '';
-  
-    photos.forEach(photo => {
-      html += `
-        <div class="carousel-item">
-          <img src="${photo.src}" alt="${photo.alt}" class="foto">
-        </div>
-      `;
+  let html = '';
+
+  photos.forEach(photo => {
+    html += `
+      <div class="carousel-item">
+        <img src="${photo.src}" alt="${photo.alt}" class="foto">
+      </div>
+    `;
+  });
+
+  return html;
+}
+
+function generateButtons(){
+
+    $.getJSON('result.json', function(jd) {
+       $('#stage').html('<p> Name: ' + jd.name + '</p>');
+       $('#stage').append('<p>Age : ' + jd.age+ '</p>');
+       $('#stage').append('<p> Sex: ' + jd.sex+ '</p>');
     });
-  
-    return html;
-  }
-  
+ };
 
 // Sample photo data
 const myPhotos = [
-	{src: 'idealic_nl/_13_0771.jpg', alt: 'Leiden', category: 'ideal'},
-	{src: 'idealic_nl/_15_0773.jpg', alt: 'Utrecht', category: 'ideal'},
-  {src: 'towers/_24_0950.jpg', alt: 'Frankfurt', category: 'towers'},
-  {src: 'geometric/_28_0953.jpg', alt: 'Drachten', category: 'geometric'},
-  {src: 'geometric/_35_0960.jpg', alt: 'Brussel', category: 'geometric'},
+  {src: './photos/_15_0773.jpg', alt: 'Utrecht', category: 'ideal'},
+  {src: './photos/_13_0771.jpg', alt: 'Leiden', category: 'ideal'},
+  {src: './photos/_24_0950.jpg', alt: 'Frankfurt', category: 'towers'},
+  {src: './photos/_28_0953.jpg', alt: 'Drachten', category: 'geometric'},
+  {src: './photos/_35_0960.jpg', alt: 'Brussel', category: 'geometric'},
 ];
-
-document.addEventListener("DOMContentLoaded", function() {
-  
-  
-  
-
-    
-});
-
 
 function updateAltText(){
   
@@ -50,5 +49,6 @@ $(document).ready(function(){
   $('#carouselExampleControls').on('slid.bs.carousel', function () {
     updateAltText();
   })
+  
 });
   
