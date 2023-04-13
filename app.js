7c59b771a9c3c17ab7297e271e6506d0ffa3d0cb
keplerfrequency <keplerfrequency@gmail.com>
2023-04-13 15:58:40 +0200

    Now gets myPhotos from json file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ function generateCarouselHTML(photos) {
   let html = '';
 
   photos.forEach(photo => {
+
+    src = "https://raw.githubusercontent.com/keplerfrequency/foto_viewer/main/" + photo.src
+
     html += `
       <div class="carousel-item">
         <img src="${photo.src}" alt="${photo.alt}" class="foto">
@@ -43,16 +46,6 @@ function generateButtons(){
 
 }
 
-
-// Sample photo data
-const myPhotos = [
-  {src: './photos/_15_0773.jpg', alt: 'Utrecht', category: 'ideal'},
-  {src: './photos/_13_0771.jpg', alt: 'Leiden', category: 'ideal'},
-  {src: './photos/_24_0950.jpg', alt: 'Frankfurt', category: 'towers'},
-  {src: './photos/_28_0953.jpg', alt: 'Drachten', category: 'geometric'},
-  //{src: './photos/_35_0960.jpg', alt: 'Brussel', category: 'geometric'},
-];
-
 function updateAltText(){
   
   const activeImg = document.querySelector('.carousel-item.active .foto');
@@ -61,14 +54,50 @@ function updateAltText(){
   
   const myDiv = document.getElementById('alt_text');
   myDiv.textContent = altText;
+
 };
 
+function carouselPosition(){
+  
+  console.log(123)
 
+  let element = document.querySelector('.carousel');
+
+  let height = window.innerHeight;
+
+  let fotoHeight = document.querySelector('.carousel-item.active .foto').clientHeight; 
+
+  divHeight = 0.8 * height
+  offset = (divHeight - fotoHeight)/2 +"px"
+  console.log(offset)
+
+  element.style.top = offset;
+
+};
+
+function generateMyPhotos(){
+
+  fetch('photos.json')
+    .then(response => response.json())
+    .then(data => {
+      myPhotos = data.map(photo => { // Assign value to the global variable
+        return {
+          src: photo.src,
+          alt: photo.alt,
+          category: photo.category
+        };
+      });
+      console.log(myPhotos);
+      callback(null, myPhotos);
+    })
+    .catch(error => {
+      callback(error, null);
+    });
+
+};
 
 document.addEventListener("click", function(){
   updateAltText();
-
-
 });
 
 
@@ -80,5 +109,25 @@ $(document).ready(function(){
     updateAltText();
   })
   
+  generateMyPhotos(function(error, myPhotos) {
+    if (error) {
+      console.error(error);
+    } else {
+      // Access the global variable `photos` here
+      console.log(myPhotos);
+    }
+  });
+
 });
   
+
+
+
+// Sample photo data
+/*const myPhotos = [
+  {src: './photos/_15_0773.jpg', alt: 'Utrecht', category: 'ideal'},
+  {src: './photos/_13_0771.jpg', alt: 'Leiden', category: 'ideal'},
+  {src: './photos/_24_0950.jpg', alt: 'Frankfurt', category: 'towers'},
+  {src: './photos/_28_0953.jpg', alt: 'Drachten', category: 'geometric'},
+  //{src: './photos/_35_0960.jpg', alt: 'Brussel', category: 'geometric'},
+];*/
