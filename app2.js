937245db178573fc7cf929862579e49fbdecee94
keplerfrequency <keplerfrequency@gmail.com>
2024-07-23 13:36:41 +0200

    Updates name of foto
    Get alt text of current foto and place it at the bottom of the photo

--- app2.js
+++ app2.js
@@ -28,17 +28,34 @@ function generateButtons(){
   
   }
 
+function findAltBySrc(src){
 
-  function updateAltText(){
-  
-  
-  };
+  fetch('photos.json')
+  .then(response => response.json())
+  .then(data => {
+    myPhotos = data.map(photo => { // Assign value to the global variable
+      return {
+        src: photo.src,
+        alt: photo.alt,
+        category: photo.category
+      };
+    });
 
-  function findAltBySrc(src) {
-    for (let i = 0; i < jsonData.length; i++) {
-        if (jsonData[i].src === src) {
-            return jsonData[i].alt;
-        }
-    }
-    return null; // Return null if src is not found
+    let altText = getAltText(src, myPhotos);
+
+    const myDiv = document.getElementById('line_breaker');
+    myDiv.textContent = altText.toUpperCase();
+
+  })
+  .catch(error => {
+    callback(error, null);
+  });
+
+
+};
+
+//Looks through the myPhotos array and returns the alt
+function getAltText(src, myPhotos) {
+  let photo = myPhotos.find(photo => photo.src === src);  
+  return photo ? photo.alt : null;
 }
